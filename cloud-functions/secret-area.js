3473f8e3d6e73dd44f7c820e02ca4e78b295fa00
Mario Gilezan <mariogilezan10@gmail.com>
2020-01-16 18:01:47 +0100

    added client area feature

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
   const secretContent = `
-    <h3>Welcome to the secret area</h3>
+    <h3>Welcome To The Secret Area</h3>
     <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
   `;
 
